e7f1cc57771db441ca772805102e4972f672a2bc
wertyuiop2024 <ptaboada@fidenzaconsultores.com>
2024-10-11 21:37:33 -0500

    finish calculator

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,116 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { globalStyles } from '@/styles/global-styles'
-import ThemeText from '@/components/ThemeText'
-import CalculatorButton from '@/components/CalculatorButton'
-import { Colors } from '@/constants/Colors'
-import { useCalculator } from '@/hooks/useCalculator'
+import CalculatorButton from '@/components/CalculatorButton';
+import ThemeText from '@/components/ThemeText';
+import { Colors } from '@/constants/Colors';
+import { useCalculator } from '@/hooks/useCalculator';
+import { globalStyles } from '@/styles/global-styles';
+import { View, Text } from 'react-native';
 
 const CalculatorApp = () => {
-
-
   const {
     formula,
+    prevNumber,
     buildNumber,
     clean,
     toggleSign,
-    deleteLast
-  } = useCalculator()
+    deleteLast,
+    divideOperation,
+    multiplyOperation,
+    subtractOperation,
+    addOperation,
+    calculateSubResult,
+    calculateResult,
+  } = useCalculator();
 
   return (
     <View style={globalStyles.calculatorContainer}>
+      {/* Resultados */}
       <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
-        <ThemeText variant='h1'>{formula}</ThemeText>
-        <ThemeText variant='h2'>250</ThemeText>
+        <ThemeText variant="h1">{formula}</ThemeText>
+
+        {formula === prevNumber ? (
+          <ThemeText variant="h2"> </ThemeText>
+        ) : (
+          <ThemeText variant="h2">{prevNumber}</ThemeText>
+        )}
+      </View>
+
+      {/* Filas de botones */}
+
+      <View style={globalStyles.row}>
+        <CalculatorButton
+          label="C"
+          blackText
+          color={Colors.lightGray}
+          onPress={clean}
+        />
+        <CalculatorButton
+          label="+/-"
+          blackText
+          color={Colors.lightGray}
+          onPress={toggleSign}
+        />
+        <CalculatorButton
+          label="del"
+          blackText
+          color={Colors.lightGray}
+          onPress={deleteLast}
+        />
+        <CalculatorButton
+          label="÷"
+          color={Colors.orange}
+          onPress={divideOperation}
+        />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton color={Colors.lightGray} onPress={clean} blackText label='C' />
-        <CalculatorButton color={Colors.lightGray} onPress={toggleSign} blackText label='+/-' />
-        <CalculatorButton color={Colors.lightGray} onPress={deleteLast} blackText label='del' />
-        <CalculatorButton color={Colors.orange} onPress={()=> buildNumber('÷')}  label='÷' />
+        <CalculatorButton label="7" onPress={() => buildNumber('7')} />
+        <CalculatorButton label="8" onPress={() => buildNumber('8')} />
+        <CalculatorButton label="9" onPress={() => buildNumber('9')} />
+        <CalculatorButton
+          label="X"
+          color={Colors.orange}
+          onPress={multiplyOperation}
+        />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('7')} label='7' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('8')} label='8' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('9')} label='9' />
-        <CalculatorButton color={Colors.orange} onPress={()=> buildNumber('x')}  label='X' />
+        <CalculatorButton label="4" onPress={() => buildNumber('4')} />
+        <CalculatorButton label="5" onPress={() => buildNumber('5')} />
+        <CalculatorButton label="6" onPress={() => buildNumber('6')} />
+        <CalculatorButton
+          label="-"
+          color={Colors.orange}
+          onPress={subtractOperation}
+        />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('4')} label='4' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('5')} label='5' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('6')} label='6' />
-        <CalculatorButton color={Colors.orange} onPress={()=> buildNumber('-')}  label='-' />
+        <CalculatorButton label="1" onPress={() => buildNumber('1')} />
+        <CalculatorButton label="2" onPress={() => buildNumber('2')} />
+        <CalculatorButton label="3" onPress={() => buildNumber('3')} />
+        <CalculatorButton
+          label="+"
+          color={Colors.orange}
+          onPress={addOperation}
+        />
       </View>
 
       <View style={globalStyles.row}>
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('1')} label='1' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('2')} label='2' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('3')} label='3' />
-        <CalculatorButton color={Colors.orange} onPress={()=> buildNumber('=')}  label='=' />
-      </View>
+        <CalculatorButton
+          label="0"
+          doubleSize
+          onPress={() => buildNumber('0')}
+        />
+        <CalculatorButton label="." onPress={() => buildNumber('.')} />
 
-      <View style={globalStyles.row}>
-        <CalculatorButton doubleSize color={Colors.darkGray} onPress={()=> buildNumber('0')} label='0' />
-        <CalculatorButton color={Colors.darkGray} onPress={()=> buildNumber('.')} label='.' />
-        <CalculatorButton color={Colors.orange} onPress={()=> buildNumber('=')}  label='=' />
+        <CalculatorButton
+          label="="
+          color={Colors.orange}
+          onPress={calculateResult}
+        />
       </View>
-      
-
     </View>
+  );
+};
 
-  )
-}
-
-export default CalculatorApp
+export default CalculatorApp;
--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,11 +1,13 @@
+import { Pressable, Text } from 'react-native';
+
+import * as Haptics from 'expo-haptics';
+
 import { Colors } from '@/constants/Colors';
 import { globalStyles } from '@/styles/global-styles';
-import { View, Text, Pressable } from 'react-native';
-import * as Haptics from 'expo-haptics';
 
 interface Props {
   label: string;
-  color: string;
+  color?: string;
   blackText?: boolean;
   doubleSize?: boolean;
   onPress: () => void;
@@ -16,30 +18,30 @@ const CalculatorButton = ({
   color = Colors.darkGray,
   blackText = false,
   doubleSize = false,
-  onPress
+  onPress,
 }: Props) => {
   return (
-    <Pressable style={({ pressed }) => ({
-      ...globalStyles.button,
-      backgroundColor: color,
-      opacity: pressed ? 0.8 : 1,
-      width: doubleSize ? 180 : 80,
-    })}
-      onPress={
-        () => {
-          Haptics.selectionAsync();
-          onPress();
-        }
-      }
+    <Pressable
+      style={({ pressed }) => ({
+        ...globalStyles.button,
+        backgroundColor: color,
+        opacity: pressed ? 0.8 : 1,
+        width: doubleSize ? 180 : 80,
+      })}
+      onPress={() => {
+        Haptics.selectionAsync();
+        onPress();
+      }}
     >
-      <Text style={{
-        ...globalStyles.buttonText,
-        color: blackText ? 'black' : 'white',
-      }}>
+      <Text
+        style={{
+          ...globalStyles.buttonText,
+          color: blackText ? 'black' : 'white',
+        }}
+      >
         {label}
       </Text>
     </Pressable>
-  )
-}
-
-export default CalculatorButton
+  );
+};
+export default CalculatorButton;
